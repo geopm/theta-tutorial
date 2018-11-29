f9a6593a4b640b378cc0f734ac5c68d679abaff6
Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>
2018-11-29 14:57:12 -0800

    Fix up exclude_nodes.sh.
    Signed-off-by: Christopher M. Cantalupo <christopher.m.cantalupo@intel.com>

--- exclude_nodes.sh
+++ exclude_nodes.sh
@@ -18,24 +18,34 @@ if [ -z "$BAD_NODES" ]; then
     NUM_BAD_NODES=0
 else
     NUM_BAD_NODES=$(echo $BAD_NODES | sed 's|[^,]||g' | wc -c)
-    if [ $NUM_REQUIRED_NODES -gt $(($COBALT_JOBSIZE - $NUM_BAD_NODES)) ]; then
-        >&2 echo "Error: number of msr-safe enabled nodes is less than number of nodes required!"
-        >&2 echo "Warning: msr-safe failure detected on the following nodes: $BAD_NODES"
-        exit 1
-    else
-        NUM_EXTRA_NODES=$(($COBALT_JOBSIZE - $NUM_BAD_NODES - $NUM_REQUIRED_NODES))
-        if [ $NUM_EXTRA_NODES -gt 0 ]; then
-            HOSTNAME=get_hostname.sh
-            echo '#!/bin/bash' > $HOSTNAME
-            echo "hostname | sed -e 's/nid[0]*//'" >> $HOSTNAME
-            echo 'true' >> $HOSTNAME
-            chmod u+x $HOSTNAME
-            EXTRA_NODES=,$(aprun -E $BAD_NODES -n $(($COBALT_JOBSIZE - $NUM_BAD_NODES)) -N1 -q ./$HOSTNAME | \
-                        sort | tail -n $NUM_EXTRA_NODES | tr '\n' ' ' | sed 's/\>/,/g;s/ //g;s/,$//')
-            rm -f $HOSTNAME
-        fi
-        echo -n "-E $BAD_NODES$EXTRA_NODES"
+fi
+
+if [ $NUM_REQUIRED_NODES -ge $(($COBALT_JOBSIZE - $NUM_BAD_NODES)) ]; then
+    >&2 echo "Error: number of msr-safe enabled nodes is less than number of nodes required!"
+    >&2 echo "Warning: msr-safe failure detected on the following nodes: $BAD_NODES"
+    exit 1
+fi
+
+NUM_EXTRA_NODES=$(($COBALT_JOBSIZE - $NUM_BAD_NODES - $NUM_REQUIRED_NODES))
+if [ $NUM_EXTRA_NODES -gt 0 ]; then
+    HOSTNAME=get_hostname.sh
+    echo '#!/bin/bash' > $HOSTNAME
+    echo "hostname | sed -e 's/nid[0]*//'" >> $HOSTNAME
+    echo 'true' >> $HOSTNAME
+    chmod u+x $HOSTNAME
+
+    if [ $NUM_BAD_NODES -gt 0 ]; then
+        EXCLUDE_OPTION="-E $BAD_NODES"
     fi
+
+    EXTRA_NODES=$(aprun $EXCLUDE_OPTION -n $(($COBALT_JOBSIZE - $NUM_BAD_NODES)) -N1 -q ./$HOSTNAME | \
+                sort | tail -n $NUM_EXTRA_NODES | tr '\n' ' ' | sed 's/\>/,/g;s/ //g;s/,$//')
+    rm -f $HOSTNAME
+fi
+
+EXCLUDE_LIST=$(echo "$BAD_NODES,$EXTRA_NODES" | sed 's/,$//;s/^,//')
+if [ ! -z "$EXCLUDE_LIST" ]; then
+    echo -n "-E $EXCLUDE_LIST"
 fi
 
 echo "NUM_BAD_NODES=$NUM_BAD_NODES" > exclude_nodes.log
